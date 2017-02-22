cf99efa69150b39fec1fff05cbe350ff2c69606d
Michael Goff <magoff2@ncsu.edu>
2017-02-21 22:50:28 -0500

    close #23 Adding user data for new user created with email signup

--- approach2/src/app/signup/signup.component.ts
+++ approach2/src/app/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
+import { AngularFire, AuthProviders, AuthMethods, FirebaseObjectObservable } from 'angularfire2';
 import { Router } from '@angular/router';
 import { moveIn, fallIn } from '../router.animations';
 
@@ -14,9 +14,9 @@ export class SignupComponent implements OnInit {
 
   state: string = '';
   error: any;
+  user: FirebaseObjectObservable<any[]>;
 
   constructor(public af: AngularFire,private router: Router) {
-
   }
 
   onSubmit(formData) {
@@ -28,6 +28,21 @@ export class SignupComponent implements OnInit {
       }).then(
         (success) => {
         console.log(success);
+        this.af.auth.subscribe(auth => {
+          if(auth) {
+            var userID = auth.uid;
+            this.user = this.af.database.object('/users/' + userID);
+            this.user.set({
+              name: formData.value.name,
+              email: formData.value.email, 
+              skills: {
+                backend: formData.value.backend,
+                frontend: formData.value.frontend,
+                database: formData.value.database
+              }
+            });
+          }
+        });
         this.router.navigate(['/members'])
       }).catch(
         (err) => {
