cb4fc8a23f4529fdaf988dc897bf647f6b1f010f
bhaskarsinha1311 <bsinha@ncsu.edu>
2017-03-14 12:48:22 -0400

    Delete exec.sh

--- exec.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "# FindTeamMate" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/nip572/FindTeamMate.git
-git push -u origin master
